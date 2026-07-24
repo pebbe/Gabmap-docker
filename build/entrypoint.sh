ab5adc5349ee83c059e1b8d6770395fc8aa244bb
Peter Kleiweg <kleiweg@ziggo.nl>
2026-07-24 17:11:08 +0200

    don't fail if groupadd or useradd fail

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -14,40 +14,40 @@ gid=`stat -c %g /mod/data`
 
 case "$1" in
     serve)
-	set -e
-	if [ "$uid" = "0" ]
-	then
-	    groupadd --system --gid 999 web
-	    useradd --system --uid 999 --gid 999 --no-create-home web
-	    cd /mod/data
-	    for i in . .queue User
-	    do
-		if [ -d $i ]
-		then
-		    chown root:web $i
-		    chmod g+sw $i
-		fi
-	    done
-	else
-	    groupadd --gid $gid web
-	    useradd --no-create-home --uid $uid --gid $gid web
-	fi
-	trap cleanexit 1 2 3 9 15
-	lighttpd -D -f /mod/etc/httpd.config &
-	cpid=$!
-	wait $cpid
-	;;
+        set -e
+        if [ "$uid" = "0" ]
+        then
+            groupadd --system --gid 999 web
+            useradd --system --uid 999 --gid 999 --no-create-home web
+            cd /mod/data
+            for i in . .queue User
+            do
+                if [ -d $i ]
+                then
+                    chown root:web $i
+                    chmod g+sw $i
+                fi
+            done
+        else
+            groupadd --gid $gid web || true
+            useradd --no-create-home --uid $uid --gid $gid web || true
+        fi
+        trap cleanexit 1 2 3 9 15
+        lighttpd -D -f /mod/etc/httpd.config &
+        cpid=$!
+        wait $cpid
+        ;;
     shell)
-	if [ "$uid" = "0" ]
-	then
-	    groupadd --system --gid 999 web
-	    useradd --system --uid 999 --gid 999 --no-create-home web
-	else
-	    groupadd --gid $gid web
-	    useradd --no-create-home --uid $uid --gid $gid web
-	fi
+        if [ "$uid" = "0" ]
+        then
+            groupadd --system --gid 999 web
+            useradd --system --uid 999 --gid 999 --no-create-home web
+        else
+            groupadd --gid $gid web || true
+            useradd --no-create-home --uid $uid --gid $gid web || true
+        fi
         echo
-        echo Beschikbare editors: nano, vim
+        echo Available editors: nano, vim
         echo
         /bin/bash --rcfile /mod/etc/profile.sh
         ;;
